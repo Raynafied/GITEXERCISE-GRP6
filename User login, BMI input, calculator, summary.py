b5496e6c4c78749434be8a880ee22f123601057f
Ratna RK <ratnak0036@gmail.com>
2024-08-31 18:40:43 +0800

    bmi category

--- User login, BMI input, calculator, summary.py
+++ User login, BMI input, calculator, summary.py
@@ -9,3 +9,14 @@ def calculate_bmi(height_cm, weight_kg):
     height_m = height_cm / 100  # Convert height from cm to meters
     bmi = weight_kg / (height_m ** 2)  # Calculate BMI
     return bmi  # Return the BMI value
+
+#  BMI Category
+def bmi_category(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif 18.5 <= bmi < 24.9:
+        return "Normal weight"
+    elif 25 <= bmi < 29.9:
+        return "Overweight"
+    else:
+        return "Obesity"
