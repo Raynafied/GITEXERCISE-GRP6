537f6318460e3bb54f9d55ff8e2e2605c8570e2c
Ratna RK <ratnak0036@gmail.com>
2024-09-14 18:15:27 +0800

    testing entrybox

--- tKnter.py
+++ tKnter.py
@@ -20,18 +20,37 @@
 #window.mainloop()
 
 # 3rd testing for buttons
+# from tkinter import*
+# def click():
+#     print ('I am fine!')
+
+# window = Tk()
+# window.geometry('500x500')
+# window.title('Female Fitness App')
+
+# label = Label(window,text='Hi')
+# label.place(x=100,y=200)
+
+# button = Button(window,text='Welcome GORGEOUS!',command=click)
+# button.place(x=200,y=300)
+
+# window.mainloop()
+
+
+# 4th testing the entrybox
 from tkinter import*
-def click():
-    print ('I am fine!')
+def submit():
+    value = entry.get()
+    print(value)
 
 window = Tk()
 window.geometry('500x500')
 window.title('Female Fitness App')
 
-label = Label(window,text='Hi')
-label.place(x=100,y=200)
+entry = Entry(window)
+entry.place(x=100,y=200)
 
-button = Button(window,text='Welcome GORGEOUS!',command=click)
-button.place(x=200,y=300)
+buttonSubmit = Button(window,text='Lets Start',command=submit)
+buttonSubmit.place(x=200,y=100)
 
 window.mainloop()
