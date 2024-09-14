4912f60c2483a4290b702f3124906d4a5512791d
Ratna RK <ratnak0036@gmail.com>
2024-09-14 18:01:11 +0800

    tKnter.py

--- tKnter.py
+++ tKnter.py
@@ -1,8 +1,20 @@
 # first part tkinter testing
+#from tkinter import*
+
+#window = Tk()
+#window.geometry('500x500')
+#window.title('Female Fitness App')
+
+#window.mainloop()
+
+#2nd testing for labels
 from tkinter import*
 
 window = Tk()
 window.geometry('500x500')
 window.title('Female Fitness App')
+
+label = Label(window,text='Welcome, Please enter your name gorgeous!')
+label.place(x=100,y=200)
 
 window.mainloop()
