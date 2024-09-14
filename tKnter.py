6820d90f6fceed8bb1c7dd18614830aa1167b600
Ratna RK <ratnak0036@gmail.com>
2024-09-14 18:06:01 +0800

    testing for buttons and placements of labels/buttons

--- tKnter.py
+++ tKnter.py
@@ -8,13 +8,30 @@
 #window.mainloop()
 
 #2nd testing for labels
+#from tkinter import*
+
+#window = Tk()
+#window.geometry('500x500')
+#window.title('Female Fitness App')
+
+#label = Label(window,text='Welcome, Please enter your name gorgeous!')
+#label.place(x=100,y=200)
+
+#window.mainloop()
+
+# 3rd testing for buttons
 from tkinter import*
+def click():
+    print ('I am fine!')
 
 window = Tk()
 window.geometry('500x500')
 window.title('Female Fitness App')
 
-label = Label(window,text='Welcome, Please enter your name gorgeous!')
+label = Label(window,text='Hi')
 label.place(x=100,y=200)
+
+button = Button(window,text='Welcome GORGEOUS!',command=click)
+button.place(x=200,y=300)
 
 window.mainloop()
